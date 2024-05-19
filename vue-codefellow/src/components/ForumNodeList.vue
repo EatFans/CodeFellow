673a2cf5d7d3c353f0fcf2a23cca2cf1c15f012a
EatFan <eatfan0921@163.com>
2024-05-19 17:37:17 +0800

    进一步完成论坛主页页面 by EatFan on 2024/5/19

--- vue-codefellow/src/components/ForumNodeList.vue
+++ vue-codefellow/src/components/ForumNodeList.vue
@@ -1,16 +1,36 @@
 <!-- 版块节点列表组件 -->
 <template>
   <div class="forum-node-list-container">
-    <ForumNode />
+    <AnnouncementNode />
+    <LanguagesNode class="node"/>
+    <WebDevelopment class="node"/>
+    <MobileDevelopment class="node"/>
+    <DatabasesNode class="node" />
+    <GameDevelopmentNode class="node" />
+    <AINode class="node" />
+    <ProblemNode class="node"  />
   </div>
 </template>
 
 <script>
-import ForumNode from "@/components/ForumNode.vue"
-
+import AnnouncementNode from "@/components/nodes/AnnouncementNode.vue"
+import LanguagesNode from "@/components/nodes/LanguagesNode.vue";
+import WebDevelopment from "@/components/nodes/WebDevelopmentNode.vue";
+import MobileDevelopment from "@/components/nodes/MobileDevelopmentNode.vue";
+import DatabasesNode from "@/components/nodes/DatabasesNode.vue";
+import GameDevelopmentNode from "@/components/nodes/GameDevelopmentNode.vue";
+import AINode from "@/components/nodes/AINode.vue";
+import ProblemNode from "@/components/nodes/ProblemNode.vue";
 export default {
   components:{
-    ForumNode
+    AnnouncementNode,
+    LanguagesNode,
+    WebDevelopment,
+    MobileDevelopment,
+    DatabasesNode,
+    GameDevelopmentNode,
+    AINode,
+    ProblemNode
   }
 }
 </script>
@@ -19,7 +39,10 @@ export default {
 .forum-node-list-container {
   width: auto;
   height: 1900px;
-  margin-top: 20px;
-  background: firebrick;
+  margin-top: 10px;
+}
+
+.node {
+  margin-top: 10px;
 }
 </style>
---
+++ vue-codefellow/src/components/nodes/MobileDevelopmentNode.vue
@@ -0,0 +1,74 @@
+<!-- 移动应用开发 -->
+<template>
+  <div class="forum-node-container">
+    <!-- 标题栏 -->
+    <div class="node-bar">
+      <div class="node-bar-content">
+        <h1 class="node-title">移动应用开发</h1>
+      </div>
+      <div class="border-bottom"></div>
+    </div>
+
+    <!-- 节点展开后容器 -->
+    <div class="node-content">
+      <!-- 新闻资讯 -->
+
+      <!-- 公告 -->
+
+      <!-- 开发讨论 -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/assets/theme.css"
+
+export default {
+  data()  {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
+<style scoped>
+/* 论坛版块节点容器样式 */
+.forum-node-container {
+  width: auto;
+}
+
+/* 节点栏样式 */
+.node-bar {
+  position: relative; /* 使子元素能够相对于此元素定位 */
+  height: 50px;
+  width: auto;
+}
+
+/* 节点栏下边框样式 */
+.border-bottom {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 5px;
+  background: var(--ui-color);
+  border-radius: 4px;
+}
+
+/* 节点标题字体样式 */
+.node-title {
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  font-size: 18px;
+  font-weight: 300;
+  color: #1c1010;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+}
+</style>
