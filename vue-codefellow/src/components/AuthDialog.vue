11569daeb41ba51cfa77cac8c3c0da9e224bfd39
EatFan <eatfan0921@163.com>
2024-06-11 04:17:04 +0800

    登录注册验证组件初步完善 by EatFan on 2024/6/11

--- vue-codefellow/src/components/AuthDialog.vue
+++ vue-codefellow/src/components/AuthDialog.vue
@@ -3,18 +3,56 @@
   <div v-show="visible" class="auth-dialog-overlay-container">
     <div class="auth-dialog-overlay-content" >
       <div class="auth-dialog-overlay-header">
-        <h3 class="title-value">登录</h3>
+        <h3 class="title-value">{{ getTitleValue() }}</h3>
         <button class="close-button" @click="closeDialog">
           <i class='bx bx-x'></i>
         </button>
+      </div>
+
+      <div class="auth-dialog-overlay-body">
+        <div class="login-box-container">
+          <div class="other-sign-in-box">
+            <p>其他登录</p>
+            <button class="github-login" @click="">
+              <i class='bx bxl-github' ></i>
+              <p>GitHub</p>
+            </button>
+            <button class="qq-login" @click="">
+
+              <p>QQ</p>
+            </button>
+          </div>
+
+          <!-- 登录盒子 -->
+          <div class="sign-in-box">
+            <form >
+              <div class="form-group">
+                <label for="username">用户名 <span>* </span></label>
+                <input type="text" id="username" v-model="signInUserName" />
+              </div>
+              <div class="form-group">
+                <label for="password">密码 <span>* </span></label>
+                <input type="password" id="password" v-model="signInPassword" />
+              </div>
+            </form>
+          </div>
+
+          <!-- 注册盒子 -->
+          <div class="register-box">
+
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Label from "@/components/Label.vue";
+
 export default {
   name: 'AuthDialog',
+  components: {Label},
   props: {
     visible: {
       type: Boolean,
@@ -29,11 +67,38 @@ export default {
       }
     }
   },
+  data(){
+    return {
+      signInUserName: '',
+      signInPassword: '',
+    }
+  },
   methods: {
+    // 触发关闭弹窗的操作
     closeDialog() {
-      // 触发关闭弹窗的操作
       this.$emit('update:visible', false);
+      this.titleValue = '';
     },
+
+    // 获取弹窗标题
+    getTitleValue(){
+      let titleValue;
+
+      switch (this.authType.toLowerCase()){
+        case 'login':
+          titleValue = '登录'
+          break;
+        case 'register':
+          titleValue = '注册';
+          break;
+        default:
+          titleValue = '';
+          break;
+
+      }
+
+      return titleValue;
+    }
   },
 
 
@@ -56,11 +121,9 @@ export default {
 
 .auth-dialog-overlay-content {
   width: 700px;
-  height: 700px;
   background: #fff;
   border-radius: 0.7rem;
   box-shadow: 0 0.9rem 1.7rem rgba(0, 0, 0, 0.25), 0 0.7rem 0.7rem rgba(0, 0, 0, 0.22);
-  overflow-y: auto; /* 当内容溢出时显示滚动条 */
 }
 
 .auth-dialog-overlay-header {
@@ -87,6 +150,87 @@ export default {
   margin-right: 10px;
   font-size: 1.4em;
   border: none;
+}
+
+.close-button:hover {
+  color: #b7b8bd;
+}
+
+.auth-dialog-overlay-body {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  border: 2px solid #1c1010;
+}
+
+.login-box-container {
+  width: 480px;
+  height: 100%;
+
+
+}
+
+.other-sign-in-box {
+  width: 100%;
+  height: 60px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  border: 1px solid #1c1010;
+}
+
+.other-sign-in-box p {
+  margin-right: 10px;
+  font-size: 14px;
+}
+
+.github-login {
+  height: 35px;
+  width: auto;
+  border-radius: 8px;
+  background: #666666;
+  border: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+}
+
+.qq-login {
+  height: 35px;
+  width: auto;
+  margin-left: 10px;
+  border-radius: 8px;
+  background: #2aa2de;
+  border: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.github-login i , .qq-login i{
+  margin-left: 10px;
+  margin-right: 5px;
+  font-size: 1.5em;
+}
+
+.github-login p, .qq-login p {
+  margin-right: 10px;
+  color: #fff;
+}
+
+.sign-in-box {
+  width: 100%;
+  height: 400px;
+  border: 1px solid #384764;
+}
+
+.form-group label span{
+  color: red;
 }
 
 .login-dialog-content {
