ff5d87154a1cef1fa6838522a65dcc6f526e9c8c
EatFan <eatfan0921@163.com>
2024-06-19 15:09:04 +0800

    设计用户业务类，实现完成用户业务 by EatFan on 2024/6/19

--- springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/controller/AuthController.java
+++ springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/controller/AuthController.java
@@ -7,15 +7,20 @@ import org.springframework.web.bind.annotation.*;
 
 
 /**
- * 验证相关API接口
+ * Auth验证相关API接口
  * author: EatFan
  */
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    /**
+     * 注册新用户
+     * @param requestBodyUser 请求中部分user json数据信息
+     * @return 返回Api请求对象
+     */
     @PostMapping    ("/register")
-    public ResponseEntity<?> registerUser(@RequestBody User requestBodyUser){
+    public ResponseEntity<?> registerNewUser(@RequestBody User requestBodyUser){
         // 检测该用户是否已经注册
 
         // 将该用户的信息创建保存到数据库
---
+++ springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/service/UserService.java
@@ -0,0 +1,8 @@
+package cn.newworld.springbootcodefellow.service;
+
+/**
+ * 用户业务类
+ * author: EatFan
+ */
+public class UserService {
+}
