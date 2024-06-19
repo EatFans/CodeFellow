9523bfe75caee52802756ebd6018e0e485e3f16a
EatFan <eatfan0921@163.com>
2024-06-19 14:13:48 +0800

    设计User数据模型类 by EatFan on 2024/6/19

---
+++ springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/model/entity/User.java
@@ -0,0 +1,16 @@
+package cn.newworld.springbootcodefellow.model.entity;
+
+import java.util.Date;
+
+public class User {
+    private String uuid;    // 用户的UUID
+    private String account;  // 用户的账号
+    private String password;    // 用户的密码
+    private String username;    // 用户的名称
+    private String email;       // 用户的邮箱
+    private String phoneNumber; // 用户的手机号
+    private String status;  // 用户账号的状态
+    private Boolean isVerification; // 账号是否已经通过验证
+    private Date registrationTime;  // 注册的时间
+    private Date lastLoginTime; // 最后登录的时间
+}
