e5858bc8df96e7520526aa5e086410ec3d1b8231
EatFan <eatfan0921@163.com>
2024-06-27 02:03:12 +0800

    前端优化完善注册页面，前端准备接入Auth相关接口 by EatFan on 2024/6/27

--- vue-codefellow/src/layouts/registerPage/RegisterPageContent.vue
+++ vue-codefellow/src/layouts/registerPage/RegisterPageContent.vue
@@ -9,18 +9,24 @@
       <div class="register-box-container">
         <div class="register-box">
           <form action="">
-            <div class="register-form-input-item">
-              <label for="register-amount">账户 <span>*</span></label>
-              <div class="input-box">
-                <input type="text" id="register-amount" name="amount" v-model="amount">
 
+            <div class="register-form-input-item" :class="{ 'input-error': error.amount }">
+              <div class="register-form-input-item-title">
+                <label for="register-amount">账户 <span>*</span></label>
+                <p v-show="error.amount">{{ error.amount }}</p>
+              </div>
+              <div class="input-box">
+                <input type="text" id="register-amount" name="amount" v-model="registerData.amount">
               </div>
               <p class="item-readme">这将作为你的账号用于登录</p>
             </div>
 
-            <div class="register-form-input-item">
-              <label for="register-password">密码 <span>*</span></label>
-              <input type="password" id="register-password" name="password" v-model="password" v-on:input="togglePasswordDetectorBoxDisplay" @blur="togglePasswordDetectorBoxNotDisplay">
+            <div class="register-form-input-item" :class="{ 'input-error': error.password }">
+              <div class="register-form-input-item-title" >
+                <label for="register-password">密码 <span>*</span></label>
+                <p v-show="error.password">{{ error.password }}</p>
+              </div>
+              <input type="password" id="register-password" name="password" v-model="registerData.password" v-on:input="togglePasswordDetectorBoxDisplay" @blur="togglePasswordDetectorBoxNotDisplay">
 
             </div>
             <!-- TODO: 密码检测窗口  -->
@@ -36,30 +42,42 @@
               </div>
             </div>
 
-            <div class="register-form-input-item">
-              <label for="register-password-sure">确定密码 <span>*</span></label>
+            <div class="register-form-input-item" :class="{'input-error' : error.passwordSure}">
+              <div class="register-form-input-item-title">
+                <label for="register-password-sure">确定密码 <span>*</span></label>
+                <p v-show="error.passwordSure">{{ error.passwordSure }}</p>
+              </div>
               <input type="password" id="register-password-sure" name="password-sure" v-model="passwordSure">
             </div>
 
-            <div class="register-form-input-item">
-              <label for="register-username">用户名 <span>*</span></label>
-              <input type="text" id="register-username" name="username" v-model="username">
+            <div class="register-form-input-item" :class="{'input-error' : error.username}">
+              <div class="register-form-input-item-title">
+                <label for="register-username">用户名 <span>*</span></label>
+                <p v-show="error.passwordSure">{{ error.username }}</p>
+              </div>
+              <input type="text" id="register-username" name="username" v-model="registerData.username">
               <p class="item-readme">这将作为你在本论坛显示的名称。选择任意你喜欢的名称吧！</p>
             </div>
 
-            <div class="register-form-input-item">
-              <label for="register-email">邮箱 <span>*</span></label>
-              <input type="email" id="register-email" name="email" v-model="email">
+            <div class="register-form-input-item" :class="{'input-error' : error.email}">
+              <div class="register-form-input-item-title">
+                <label for="register-email">邮箱 <span>*</span></label>
+                <p v-show="error.email">{{ error.email }}</p>
+              </div>
+              <input type="email" id="register-email" name="email" v-model="registerData.email">
             </div>
 
-            <div class="register-form-input-item">
-              <label for="register-phoneNumber">手机号 <span>*</span></label>
-              <input type="tel" id="register-phoneNumber" name="phoneNumber" v-model="phoneNumber">
+            <div class="register-form-input-item" :class="{'input-error' : error.phoneNumber}">
+              <div class="register-form-input-item-title">
+                <label for="register-phoneNumber">手机号 <span>*</span></label>
+                <p v-show="error.phoneNumber">{{ error.phoneNumber }}</p>
+              </div>
+              <input type="tel" id="register-phoneNumber" name="phoneNumber" v-model="registerData.phoneNumber">
             </div>
 
             <div class="register-form-input-item">
               <label for="register-referrer">推荐人 </label>
-              <input type="text" id="register-referrer" name="referrer" v-model="referrer">
+              <input type="text" id="register-referrer" name="referrer" v-model="registerData.referrer">
               <p class="item-readme">你的推荐人的用户名称。（选填）</p>
             </div>
 
@@ -82,10 +100,10 @@
                 <label for="rules">我自愿遵守本论坛 <a href="#">用户行为规范</a> </label>
               </div>
             </div>
-
+            
 
             <div class="register-form-button-item">
-              <input type="button" value="注册" @click="test">
+              <input type="button" value="注册" @click="register">
             </div>
           </form>
         </div>
@@ -110,37 +128,73 @@ export default {
   },
   data(){
     return {
-      amount: '',
-      password: '',
-      passwordSure: '',
-      username: '',
-      email: '',
-      phoneNumber: '',
-      referrer: '',
-      problemVerification: '',
+      // 注册数据
+      registerData: {
+        amount: '',
+        password: '',
+        username: '',
+        email: '',
+        phoneNumber: '',
+        recommender: '',
+      },
 
-      negotiate: '',
+      passwordSure: '', // 密码二次确定
+      problemVerification: '', // 问题验证
+      negotiate: '',  
       rules: '',
 
       // 密码检测窗口标识
-      passwordDetectorVisible: false
+      passwordDetectorVisible: false,
+
+      error: {
+        amount: '',
+        password: '',
+        passwordSure: '',
+        username: '',
+        email: '',
+        phoneNumber: '',
+      }
     }
   },
   methods:{
-    test(){
-      console.log(this.amount);
-      console.log(this.password);
-      console.log(this.passwordSure);
-      console.log(this.username);
-      console.log(this.email);
-      console.log(this.phoneNumber);
-      console.log(this.referrer);
-      console.log(this.problemVerification);
-      console.log(this.negotiate);
-      console.log(this.rules);
-    },
+    // 注册函数
     register(){
-      
+      // 检查输入的账号是不是为空
+      if (!this.registerData.amount){
+        this.error.amount = '账号这里不能为空！！！';
+      } else {
+        this.error.amount = '';
+      }
+      // 检查输入的密码是不是为空
+      if (!this.registerData.password){
+        this.error.password = '密码这里不能为空！！！';
+      } else {
+        this.error.password = '';
+      }
+      // 检查输入的二次确定密码是不是为空
+      if (!this.passwordSure){
+        this.error.passwordSure = '二次确定密码这里不能为空！！！';
+      } else {
+        this.error.passwordSure = '';
+      }
+      // 检查输入的用户名是不是为空
+      if (!this.registerData.username){
+        this.error.username = '用户名这里不能为空！！！';
+      } else {
+        this.error.username = '';
+      }
+      // 检查输入的邮箱是不是为空
+      if (!this.registerData.email){
+        this.error.email = '邮箱这里不能为空！！！';
+      } else {
+        this.error.email = '';
+      }
+      // 检查输入的手机号是不是为空
+      if (!this.registerData.phoneNumber){
+        this.error.phoneNumber = '手机号这里不能为空！！！';
+      } else {
+        this.error.phoneNumber = ''
+      }
     },
     togglePasswordDetectorBoxDisplay(){
       this.passwordDetectorVisible = true;
@@ -154,6 +208,19 @@ export default {
 </script>
 
 <style scoped>
+
+.register-form-input-item-title p{
+  font-size: 12px;
+  margin-left: 10px;
+  margin-top: 1px;
+  color: red;
+}
+
+.register-form-input-item-title {
+  display: flex;
+  flex-direction: row;
+}
+
 .password-detector-box{
   display: flex;
   flex-direction: column;
@@ -168,13 +235,13 @@ export default {
   margin: 80px auto 80px;
   height: 900px;
 
-  //background: #7db92e;
+  /* //background: #7db92e; */
 }
 
 .register-page-content-header {
   width: 100%;
 
-  //background: #00bd7e;
+  /* //background: #00bd7e; */
 }
 
 .register-page-content-body {
@@ -195,8 +262,9 @@ export default {
   display: flex;
   justify-content: center;
 
-  //background: #cc4242;
+  /* background: #cc4242; */
 }
+
 
 .register-form-input-item {
   width: 350px;
@@ -230,6 +298,10 @@ export default {
   font-size: 15px;
 }
 
+.input-error input {
+  outline: 1px solid red;
+}
+
 .register-form-input-item input:focus {
   outline: 1px solid #2174f1;
 }
@@ -257,6 +329,12 @@ export default {
   border: none;
   border-radius: 4px;
   background: #7db92e;
+  color: #fff;
+
+}
+
+.register-form-button-item input:hover {
+  background: #a9da6a;
   color: #fff;
 
 }
@@ -290,6 +368,8 @@ export default {
   position: absolute;
   border-radius: 0.7rem;
   box-shadow: 0 0.9rem 1.7rem rgba(0, 0, 0, 0.25), 0 0.7rem 0.7rem rgba(0, 0, 0, 0.22);
-  //border: 1px solid black;
+  /*border: 1px solid black; */
+
 }
+
 </style>
