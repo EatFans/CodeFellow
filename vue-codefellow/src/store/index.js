ea7a7575e9e05c0c9da78e0dc3ced959898a386b
EatFan <eatfan0921@163.com>
2024-06-30 11:51:52 +0800

    前端在渲染页面组件前将数据初始化好，完成！ by EatFan on 2024/6/30

--- vue-codefellow/src/store/index.js
+++ vue-codefellow/src/store/index.js
@@ -1,10 +1,12 @@
 // Vuex Store 状态
 import { createStore } from "vuex";
 import settings from "@/store/modules/settings.js";
+import user from "./modules/user";
 
 const store = createStore({
     modules: {
         settings,
+        user
     }
 });
 
