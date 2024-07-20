0dde516e51fa7ba7ea5755254641bfc64a0668c7
EatFan <eatfan0921@163.com>
2024-07-20 18:07:23 +0800

    备份，开始设计帖子与评论数据结构和数据库存储 by EatFan on 2024/7/20

---
+++ springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/model/entity/Comment.java
@@ -0,0 +1,11 @@
+package cn.newworld.springbootcodefellow.model.entity;
+
+import lombok.Data;
+
+/**
+ * 论坛评论数据对象
+ * author: EatFan
+ */
+@Data
+public class Comment {
+}
--- springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/model/entity/Post.java
+++ springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/model/entity/Post.java
@@ -3,6 +3,7 @@ package cn.newworld.springbootcodefellow.model.entity;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * 帖子数据对象类
@@ -10,16 +11,35 @@ import java.util.Date;
  */
 @Data
 public class Post {
+    // 帖子的基础数据
     private String postUUID;  // 帖子 uuid
-    private String section;  // 帖子版块
-    private String authorUUID;   // 帖子作者uuid
-    private String authorName;  // 帖子作者名字
     private String title;  // 帖子标题
-    private String[] labels;  // 帖子标签
     private String readme;    // 帖子自述介绍
     private String content;  // 帖子内容
     private String coverUrl;  // 帖子封面图片url
-    private long likeAmount;  // 帖子被点赞数量
-    private long dislikeAmount; // 帖子被踩数量
-    private Date updateDate;   // 帖子更新时间
+    private String authorUUID;   // 帖子作者uuid
+    private String authorName;  // 帖子作者名字
+    private Date createAt;  // 帖子创建时候的时间戳
+    private Data updateAt; // 帖子更新时候的时间戳
+
+    // 帖子分类和标签
+    private List<String> labels;  // 帖子标签
+    private String sectionType;  // 帖子版块类型
+
+    // 帖子互动信息
+    private long replyCount;  // 帖子的回复量
+    private long viewCount;   // 帖子浏览量
+    private long likeCount;  // 帖子点赞量
+    private long dislikeCount;  // 帖子被踩量
+    private long shareCount;  // 帖子分享量
+
+    // 帖子状态
+    private String status; // 帖子的状态
+    private boolean isPinned; // 帖子是否为置顶
+    private boolean isFeatured;  // 帖子是否为精华
+    private boolean isLocked;   // 帖子是否为锁定
+
+    // 帖子评论区
+    private List<Comment> comments;
+
 }
