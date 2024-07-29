8924b4579407530eb2b73c42897801e130daadae
EatFan <eatfan0921@163.com>
2024-07-29 22:33:40 +0800

    备份 by EatFan on 2024/7/29

---
+++ springboot-codefellow/src/main/java/cn/newworld/springbootcodefellow/service/base/AuthRequestHandler.java
@@ -0,0 +1,12 @@
+package cn.newworld.springbootcodefellow.service.base;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * auth请求处理器
+ * author: EatFan
+ */
+@Service
+public class AuthRequestHandler extends RequestHandler{
+
+}
